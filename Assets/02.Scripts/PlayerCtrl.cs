8fdb8dae5b8b26893698e7e2ffcfcfb2f73bf4ae
1y3eo <hyeo131@naver.com>
2023-08-19 23:28:56 +0900

    체력바 추가

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class PlayerCtrl : MonoBehaviour
 {
@@ -17,6 +19,7 @@ public class PlayerCtrl : MonoBehaviour
 
     private readonly float initHp = 100.0f;
     public float currHp;
+    private Image hpBar;
 
     // 델리게이트 선언
     public delegate void PlayerDieHandler();
@@ -25,6 +28,7 @@ public class PlayerCtrl : MonoBehaviour
 
     IEnumerator Start()
     {
+        hpBar = GameObject.FindGameObjectWithTag("HP_BAR")?.GetComponent<Image>();
         currHp = initHp;
 
         // 컴포넌트를 추출해 변수에 대입
@@ -88,6 +92,8 @@ public class PlayerCtrl : MonoBehaviour
         if (currHp >= 0.0f && other.CompareTag("PUNCH"))
         {
             currHp -= 10.0f;
+            DisplayHealth();
+
             Debug.Log($"Player hp = {currHp / initHp}");
 
             if (currHp <= 0.0f)
@@ -108,6 +114,11 @@ public class PlayerCtrl : MonoBehaviour
         //     monster.SendMessage("OnPlayerDie", SendMessageOptions.DontRequireReceiver);
         // }
         OnPlayerDie();
+    }
+
+    void DisplayHealth()
+    {
+        hpBar.fillAmount = currHp / initHp;
     }
 
 }
